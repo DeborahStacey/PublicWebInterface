b0b3765c8b2d1a55846f74d37b4e6d777dd10020
Alexander Gontcharov <gontchaa@mail.uoguelph.ca>
2016-12-01 03:07:09 -0500

    Added new files for Jest.

---
+++ __tests__/login.react-test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import LoginPage from '../src/components/pages/Login';
+import renderer from 'react-test-renderer';
+
+describe('Get initial state', () => {
+	it('returns someting', () => {
+		expect(getInitialState()).toBeDefined();
+	});	
+});
