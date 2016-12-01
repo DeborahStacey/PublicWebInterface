a2287d8e57eed2baee09b5b8e159b59a5020eb7e
JasonWang7 <jasonwang@JasonW.local>
2016-12-01 06:57:05 -0500

    Edit FAQ page

--- src/components/pages/dashboard/FAQ/FAQ.jsx
+++ src/components/pages/dashboard/FAQ/FAQ.jsx
@@ -13,22 +13,21 @@ var Blank = React.createClass({
            <br/>
 
           <font size="5" color="black" face="times">Q: Why is WellCat useful? </font> <br />
-          <p>A: WellCat is an important tool for the health of cats around the World. 50% of cats never get to see a veterinarian. 
+          <font size="4">A: WellCat is an important tool for the health of cats around the World. 50% of cats never get to see a veterinarian. 
           The information that you, the users, will be putting onto WellCat could potentially provide data that we may have never known about cat health and the signs that they are getting sick. 
-          This service is easily updateable and can help cat professionals and owners improve the health of cats everywhere.</p>
+          This service is easily updateable and can help cat professionals and owners improve the health of cats everywhere.</font>
           <br/><br/>
 
           <font face="times" size="5" color="black">Q: What can I do on WellCat? </font> <br />
-          <p>A: On WellCat, you can provide important information on your cats with cat portfolios. 
-          You can visit other users' profiles and read information about cats.</p>
+          <font size="4">A: On WellCat, you can provide important information on your cats with cat portfolios. 
+          You can visit other users' profiles and read information about cats.</font>
           <br/><br/>
 
           <font face="times" size="5" color="black ">Q: How public is my information? </font> <br />
-          <p>A: You can choose to make all of your info private to other users or just block certain people you don't want on your profile. 
-          Only your cat's information is stored in our WellCat databases.</p>
+          <font size="4">A: You can choose to make all of your info private to other users or just block certain people you don't want on your profile. 
+          Only your cat's information is stored in our WellCat databases.</font>
           <br/><br/>
 
-          <br/><br/> 
         </Jumbotron> 
       </div>
     );
