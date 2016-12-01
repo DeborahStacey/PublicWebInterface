8296993bdc9d53ceb2fb7d491af2fdf0406bf0da
Alexander Gontcharov <gontchaa@mail.uoguelph.ca>
2016-11-30 23:34:33 -0500

    Added Carousel to Overview Page

--- src/components/pages/dashboard/Overview/Overview.jsx
+++ src/components/pages/dashboard/Overview/Overview.jsx
@@ -1,79 +1,45 @@
 import React, { PropTypes, Component } from 'react';
 import { Link } from "react-router";
-import {Jumbotron} from 'react-bootstrap';
+import {Jumbotron, Carousel} from 'react-bootstrap';
 
 var Blank = React.createClass({
-  getInitialState: function(){
-    return {
-      loginID: '',
-      password: '',
-      isSubmitted: false
-    };
-  },
 
   render: function() {
     return (
       <div className="overview-page" key="overview"> 
         <Link to="/dashboard/overview" className="pull-right btn btn-primary btn-outline btn-rounded">About Us</Link> 
         <h2>Home</h2> 
-        <Jumbotron> 
+        <Jumbotron>
           <h1>Welcome to WellCat!</h1> 
           The Public Web Interface
-          <br /><br /> 
-        </Jumbotron> 
+        </Jumbotron>
+
+         <Carousel>
+          <Carousel.Item>
+            <img width={1500} height={500} alt="900x500" src="https://wallpprs.media/1500x500/hidden-cat-in-winter_wallpprs.cpm_.jpg"/>
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width={1500} height={500} alt="900x500" src="https://stormideaswus.blob.core.windows.net/headerjunction/2014/91/0d33a55e-b5f4-4234-ad37-8b6b44ba0f0b.jpg"/>
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width={1500} height={500} alt="900x500" src="http://www.kittycatter.com/wp-content/uploads/2016/05/cat-ready-to-eat-1500x500.jpeg"/>
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </div>
     );
   }
-  /*setLoginID: function(e) {
-    this.setState({
-      loginID: e.target.value,
-      loginError: ''
-    });
-  },
-  handleLogin: function(e){
-    var dataObject = {'email': this.state.loginID, 'password': this.state.password};
-    var data = JSON.stringify(dataObject, null, '\t');
-    
-    //Download function called here
-    //this.download(data, 'JSON.txt', 'text/plain');
-    
-    var form = new FormData();
-    form.append("email", this.state.loginID);
-    form.append("password", this.state.password);
-
-    var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://cat.ddns.net/Backend/api.php/user/login",
-      "method": "POST",
-      "headers": {},
-      "processData": false,
-      "contentType": false,
-      "mimeType": "multipart/form-data",
-      "data": form
-    }
-
-    $.ajax(settings).done(function (response) {
-      var resultsList = JSON.parse(response);
-      if (resultsList["success"] == true) {
-        console.log("Logged in");
-        document.cookie='session=Logout;path=/;';
-        
-      }
-      else{
-        console.log("Invalid Login");
-        //invalid credentials (invalid login)
-        // display error
-      };
-    });
-
-    e.preventDefault();
-    this.props.history.pushState(null, '/dashboard/overview');
-  
-    //this.transitionTo('dashboard');
-    return false;
-  }*/
-
 });
 
 export default Blank;
